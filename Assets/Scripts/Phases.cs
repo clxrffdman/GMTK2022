5dfdb09696bdc1eba1c682eba0a379b755c9d536
fnkzhang <fz82502@gmail.com>
2022-07-15 19:50:07 -0700

    test

--- Assets/Scripts/Phases.cs
+++ Assets/Scripts/Phases.cs
@@ -1,18 +1,47 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Phases : MonoBehaviour
 {
+    public float phaseTime;
+    public float buffer;
+    public Combat todo;
+    private int currentState = 0;
+    int stateIndex;
+    public Dictionary <int, Func<int, int>> whatdo = new Dictionary<int, Func<int, int>>()
+    {
+        {0, todo.Attack<int, int>},
+        {1, todo.Defense},
+        {2, todo.Utility}
+    };
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(PhaseStuff());
     }
-
+    IEnumerator PhaseStuff()
+    {
+        while(true)
+        {
+            //rolldicefunctionhere
+            yield return new WaitForSeconds(phaseTime);
+            currentState++;
+            if(currentState > 2)
+                currentState = 0;
+            whatdo[currentState](1, 2);
+            //for animations?
+            yield return new WaitForSeconds(buffer);
+        }
+    }
     // Update is called once per frame
     void Update()
     {
         
+    }
+    void Attack(int roll, int score)
+    {
+
     }
 }
