9474d867579dfc9d096259434fa46587106d9997
Calex Raffield <clxrffdman@gmail.com>
2022-07-15 18:45:06 -0700

    added textmeshpro

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,12 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
 
+    public GameObject nextPseudoball;
+    public int ballAmount;
     public PinballController currentPinball;
+    public float totalPoints;
+    public TextMeshProUGUI pointText;
 
 
     private void Awake()
@@ -30,10 +35,47 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        UpdateUIText();
+    }
+
+    void UpdateUIText()
+    {
+        pointText.text = currentPinball.currentPoints + "";
     }
 
     public void DoAttack()
+    {
+
+    }
+
+
+
+    public void ResetBall()
+    {
+        if(ballAmount > 0)
+        {
+            ballAmount--;
+            if (nextPseudoball != null)
+            {
+                Destroy(nextPseudoball);
+
+            }
+
+            currentPinball.currentPoints = 0;
+        }
+        else
+        {
+            Defeat();
+        }
+        
+    }
+
+    public void Defeat()
+    {
+
+    }
+
+    public void Victory()
     {
 
     }
